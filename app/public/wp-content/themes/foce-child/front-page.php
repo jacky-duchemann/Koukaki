8367a6d03d1d6d26e7970d33e34cda903481426d
jacky-duchemann <jackyduchemann@hotmail.fr>
2024-07-05 12:28:31 +0400

    Ajout responsive banner + rotation fleur + section oscar

--- app/public/wp-content/themes/foce-child/front-page.php
+++ app/public/wp-content/themes/foce-child/front-page.php
@@ -71,9 +71,10 @@ get_header();
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
             </div>
-            </section>
-
+        </section>
+        
         <!-- ajouter la section pour les oscars ici !! -->
+         <?php get_template_part('template-parts/section-oscar'); ?>
     </main><!-- #main -->
 
 <?php
---
+++ app/public/wp-content/themes/foce-child/template-parts/section-oscar.php
@@ -0,0 +1,8 @@
+<!--
+Template Name: Section nomination aux oscars
+-->
+
+<div class="oscar-section">
+    <h3>Fleurs d'oranger & chats errants est nominé aux oscars Short Film Animated de 2022!</h3>
+    <img src="<?php echo get_template_directory_uri() . '/assets/images/oscar.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
+</div>
